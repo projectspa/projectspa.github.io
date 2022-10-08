16e40d4c88962a11a444357fe75a210218ec2d6f
Jaaon <jingxuanshan@hotmail.com>
2022-10-08 15:50:43 +0800

    arrangements

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,16 @@
 window.onload=function() {
-    var h1=document.getElementById("h1");
-    var h2=document.getElementById("h2");
-    var h3=document.getElementById("h3");
-    play(h1);
-    setTimeout(function() { play(h2); },1500);
-    setTimeout(function() { play(h3); },3000);
+    var img1=document.getElementById("img1");
+    var img2=document.getElementById("img2");
+    var img3=document.getElementById("img3");
+    var head1=document.getElementById("head1");
+    var head2=document.getElementById("head2");
+    var head3=document.getElementById("head3");
+    play(img1);
+    setTimeout(function() { play(img3); },1000);
+    setTimeout(function() { play(img2); },2000);
+    setTimeout(function() { play(head1); },3000);
+    setTimeout(function() { play(head2); },4500);
+    setTimeout(function() { play(head3); },6000);
 }
 
 function play(v) {
