80e2342ec326e57ba4a24f1a0f5f29007d7a32c1
Jaaon <jingxuanshan@hotmail.com>
2022-10-08 14:37:11 +0800

    animations

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
-alert("welcome!");
+window.onload=function() {
+    var h1=document.getElementById("h1");
+    var h2=document.getElementById("h2");
+    var h3=document.getElementById("h3");
+    play(h1);
+    setTimeout(function() { play(h2); },1500);
+    setTimeout(function() { play(h3); },3000);
+}
+
+function play(v) {
+    v.className="animate";
+}
